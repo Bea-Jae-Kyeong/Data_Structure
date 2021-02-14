29f6fb2fbeac4b1813e53693f4510bd799ae38ce
JKB <nonsig@naver.com>
2021-02-15 01:14:51 +0900

    Update func2.cpp

--- 0x03/func2.cpp
+++ 0x03/func2.cpp
@@ -1,5 +1,5 @@
 //둘이 합해 100이 되는 원소가 있을 경우 1 아니면 0 출력하는 함수
-
+//O(N^2)
 int func2(int arr[], int N){
   int arr[101] = {};
   for(int i = 0; i < N; i++){
@@ -7,6 +7,17 @@ int func2(int arr[], int N){
       if(100-arr[i] == arr[j])
         return 1;
     }
+  }
+  return 0;
+}
+
+//O(N)
+int func2(int arr[], int N){
+  int occur[101] = {};
+  for(int i = 0; i < N; i++){
+    if(occur[100-arr[i]] == 1)
+      return 1;
+    occur[arr[i]] = 1;
   }
   return 0;
 }
