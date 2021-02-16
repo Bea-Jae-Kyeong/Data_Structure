2cd69faceca90a4ee3f05536109ca758131d2498
JKB <nonsig@naver.com>
2021-02-16 15:43:02 +0900

    Update Linked_list.cpp

--- Linked_list.cpp
+++ Linked_list.cpp
@@ -6,11 +6,25 @@ int dat[MX], pre[MX], nxt[MX];
 int unused = 1;
 
 void insert(int addr, int num){
+  if(addr==0){
+    dat[0]=num;
+    nxt[0]=pre[1];
+  }
+  else{
+    
+nxt[addr-1]=pre[addr];
+    pre[addr]=nxt[addr-1];
+    dat[addr]=num;
+    pre[addr+1]=nxt[addr];
+    nxt[addr]=pre[addr+1];
+    
+  }
 
 }
 
 void erase(int addr){
-
+  nxt[addr-1]=pre[addr+1];
+  pre[addr+1]=nxt[addr-1];
 }
 
 void traverse(){
